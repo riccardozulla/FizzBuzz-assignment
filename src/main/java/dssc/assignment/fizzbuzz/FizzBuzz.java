5d5505f6593341cbd10a36abe319600f31309572
Riccardo Zulla <riccardozulla@gmail.com>
2021-11-12 16:48:53 +0100

    Complete extended version

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,15 @@ package dssc.assignment.fizzbuzz;
 
 public class FizzBuzz {
 
-    public static void main(String... args) {
-        
-    }
+  public static void main(String... args) {
+    int upperBound = 105;
+    for (int i = 1; i < upperBound; ++i) {
+      String test = "";
+      test += i % 3 == 0 ? "Fizz" : "";
+      test += i % 5 == 0 ? "Buzz" : "";
+      test += i % 7 == 0 ? "Bang" : "";   //added for extended version
 
+      System.out.println(test.isEmpty() ? i : test);
+    }
+  }
 }

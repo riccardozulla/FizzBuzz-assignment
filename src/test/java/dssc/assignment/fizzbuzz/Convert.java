1c1843aec15bf794dad9612eeca02caf698a9fd9
Riccardo Zulla <riccardozulla@gmail.com>
2021-11-19 10:46:35 +0100

    Convert 5 to string 'Buzz'

--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -10,7 +10,7 @@ public class Convert {
 
   @ParameterizedTest
   @CsvSource({"1, 1", "2, 2", "40, 40"})
-  void numbers_not_divisible_by_3_and_5(int number, String expected) {
+  void multiple_of_3_and_5(int number, String expected) {
     FizzBuzz fizzBuzz = new FizzBuzz();
     Assertions.assertEquals(expected, fizzBuzz.convert(number));
   }
@@ -20,5 +20,11 @@ public class Convert {
   void numbers_divisible_by_3_to_Fizz(int number) {
     FizzBuzz fizzBuzz = new FizzBuzz();
     Assertions.assertEquals("Fizz", fizzBuzz.convert(number));
+  }
+
+  @Test
+  void number_5_to_Buzz() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    Assertions.assertEquals("Buzz", fizzBuzz.convert(5));
   }
 }

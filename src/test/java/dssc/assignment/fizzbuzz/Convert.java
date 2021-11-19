84655599541c9da1647e3495969f81bc65395b1e
Riccardo Zulla <riccardozulla@gmail.com>
2021-11-19 10:19:10 +0100

    Refactor tests

---
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -0,0 +1,18 @@
+package dssc.assignment.fizzbuzz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.concurrent.TimeUnit;
+
+public class Convert {
+
+  @ParameterizedTest
+  @CsvSource({"1, 1", "2, 2", "40, 40"})
+  void numbers_not_divisible_by_3_and_5(int number, String expected) {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    Assertions.assertEquals(expected, fizzBuzz.convert(number));
+  }
+}
